338ed1fcd0ef21d8fa0fdd990e52622a9dea06a6
Lena Kozlova <lenaklenina1991@gmail.com>
2020-09-29 21:19:06 +0300

    feat: typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (!array || !(array instanceof Array) || array.length == 0)
+		return 0;
+	
+	//let list = [array];
+	return array.reduce((min, current) => { return min < current ? min : current; }, array[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !(array instanceof Array) || array.length == 0)
+		return 0;
+	
+	//let list = [array];
+	return array.reduce((max, current) => { return max > current ? max : current; }, array[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !(array instanceof Array) || array.length == 0)
+		return 0;
+	
+	//let list = [array];
+	return array.reduce((sum, current) => { return sum + current; }, 0) / array.length;
 }
